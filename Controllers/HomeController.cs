34106935ec8edf1cbce6e83d48310e1d69735d67
Iury Chagas <iurychagas@icloud.com>
2020-02-18 20:12:03 -0300

    add events page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Event_Hub.Controllers
         {
             return View();
         }
+        public IActionResult Events()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
