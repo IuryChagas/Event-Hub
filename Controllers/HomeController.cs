ac0c80d9452a608ccc0589a7f6544479db1eb40f
Iury Chagas <iurychagas@icloud.com>
2020-02-18 20:02:38 -0300

    add clubs locale

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Event_Hub.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Clubs()
         {
             return View();
         }
