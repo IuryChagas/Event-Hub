29cd6ab8a1bd7a513f2efba1d65d0e35f9877278
Iury Chagas <iurychagas@icloud.com>
2020-02-20 11:45:45 -0300

    sync club data with database

--- Controllers/ClubsController.cs
+++ Controllers/ClubsController.cs
@@ -1,8 +1,15 @@
 using Event_Hub.Models;
 using Microsoft.AspNetCore.Mvc;
+using Event_Hub.Data;
 
 namespace Event_Hub.Controllers {
     public class ClubsController : Controller {
+
+        private readonly ApplicationDbContext eventdb;
+        public ClubsController(ApplicationDbContext database)
+        {
+            this.eventdb = database;
+        }
         public IActionResult Index () {
             return View ();
         }
@@ -11,7 +18,9 @@ namespace Event_Hub.Controllers {
         }
         [HttpPost]
         public IActionResult Save (Club club){
-            return Content("Saved!");
+            eventdb.Clubs.Add(club);
+            eventdb.SaveChanges();
+            return Content("Club registered successfully!");
         }
 
     }
