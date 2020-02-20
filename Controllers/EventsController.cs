89520562c1186351e72996e1e915638c6afd6e8c
Iury Chagas <iurychagas@icloud.com>
2020-02-20 11:46:33 -0300

    sync Event data with database

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -1,8 +1,15 @@
 using Event_Hub.Models;
 using Microsoft.AspNetCore.Mvc;
+using Event_Hub.Data;
 
 namespace Event_Hub.Controllers {
     public class EventsController : Controller {
+
+        public readonly ApplicationDbContext eventdb;
+        public EventsController(ApplicationDbContext database)
+        {
+            this.eventdb = database;
+        }
         public IActionResult Index () {
             return View ();
         }
@@ -12,7 +19,9 @@ namespace Event_Hub.Controllers {
         
         [HttpPost]
         public IActionResult Save (Event events){
-            return Content("Saved!");
+            eventdb.Events.Add(events);
+            eventdb.SaveChanges();
+            return Content("Event successfully published!");
         }
     }
 }
