6c3d0dbcec3056dd38483da6a83d7c9187a29f24
Iury Chagas <iurychagas@icloud.com>
2020-02-21 11:26:31 -0300

    add Edit event data

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -18,10 +18,23 @@ namespace Event_Hub.Controllers {
         public IActionResult Create () {
             return View ();
         }
-        
+        public IActionResult Edit (int id) {
+            Event events = eventdb.Events.First(register => register.Id == id);
+            return View ("Create", events);
+        }
         [HttpPost]
         public IActionResult Save (Event events){
-            eventdb.Events.Add(events);
+            if (events.Id == 0)
+            {
+                eventdb.Events.Add(events);
+            }else{
+                Event eventDatabase = eventdb.Events.First(register => register.Id == events.Id);
+                eventDatabase.Title = events.Title;
+                eventDatabase.Price = events.Price;
+                eventDatabase.ReleaseDate = events.ReleaseDate;
+                eventDatabase.EventPlace = events.EventPlace;
+                eventDatabase.Units = events.Units;
+            }
             eventdb.SaveChanges();
             return RedirectToAction("Index");
         }
