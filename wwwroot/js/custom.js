d517b25f060a56f96977b20b8286acb82f44c92b
Iury Chagas <iurychagas@icloud.com>
2020-02-26 19:34:17 -0300

    add custom css --extreme gohorse

---
+++ wwwroot/js/custom.js
@@ -0,0 +1,7 @@
+<!-- Core -->
+<script src="/assets/vendor/jquery/jquery.min.js"></script>
+<script src="/assets/vendor/popper/popper.min.js"></script>
+<script src="/assets/vendor/bootstrap/bootstrap.min.js"></script>
+
+<!-- Theme JS -->
+<script src="/assets/js/argon-design-system.min.js"></script>
